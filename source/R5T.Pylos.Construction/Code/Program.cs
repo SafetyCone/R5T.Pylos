d0f0eb1c02da8730ed56a501b4c93c8c8e40d8dd
MinexAutomation <minex.automation@gmail.com>
2019-12-05 13:46:48 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Pylos.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Pylos.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
